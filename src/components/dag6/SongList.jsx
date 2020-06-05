869de0bda319b120fdd3e48234ea21467beb947f
David Andersson <lejonmanen@gmail.com>
2020-06-05 09:37:08 +0200

    Övning 6b, dag 6

--- src/components/dag6/SongList.jsx
+++ src/components/dag6/SongList.jsx
@@ -3,7 +3,13 @@ import React, { useState } from 'react';
 
 const SongList = ({ songs }) => {
     const [searchString, setSearchString] = useState('');
-    const filteredList = songs.filter(song => song.title.toLowerCase().includes(searchString.toLowerCase()));
+    const filteredList = songs.filter(song => {
+        const search = searchString.toLowerCase();
+        let titleMatches  = song.title.toLowerCase().includes(search)
+        let artistMatches = song.artist.toLowerCase().includes(search)
+        let dateMatches   = song.date.toLowerCase().includes(search)
+        return titleMatches || artistMatches || dateMatches;
+    });
     return (
         <div>
             <input type="text" placeholder="Search for a song..."
